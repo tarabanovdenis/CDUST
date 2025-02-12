1562b4a525b1e07f50948f374bebe4eec8144192
tarabanovdenis <tarabanov.denis@gmail.com>
2025-02-12 00:36:49 -0700

    some minor fixes

--- js/client.js
+++ js/client.js
@@ -4,66 +4,51 @@ window.onload = function(){
     document.getElementsByTagName('body')[0].className = '';
 
     // tabs & popups
-    document.querySelectorAll('[data-switcher]').forEach((switcher) =>{
-        switcher.addEventListener('click', function (e) {
-            e.preventDefault();
-            let switcherRequest = switcher.getAttribute('data-switcher');
-            if (this.hasAttribute('data-tab')) {
-                document.querySelectorAll('[data-tab]').forEach((tabs, i) =>{
-                    tabs.classList.remove('js-open');
-                    tabs.classList.add('button_muted');
-                    this.classList.remove('button_muted');
-                    document.querySelectorAll('[data-tab-content]').forEach((tabsContent, i) =>{
-                        tabsContent.classList.remove('js-open');
-                    });
-                });
+    document.querySelectorAll("[data-href]").forEach(href => {
+        href.addEventListener("click", function () {
+            const group = this.dataset.group;
+            const target = this.dataset.href;
+
+            if (target === "close-popup") {
+                // Remove all active classes in this group
+                document.querySelectorAll(`[data-group="${group}"]`).forEach(el => el.classList.remove("js-active"));
+                return;
             }
-            // document.querySelectorAll('[data-switcher-content]').forEach((allTabs, i) =>{
-            //     allTabs.classList.remove('js-open');
-            // });
-            document.querySelectorAll('[data-switcher-content='+switcherRequest+']')[0].classList.add('js-open');
-        });
-    });
-    document.querySelectorAll('.js-close-popup').forEach((closePopup) =>{
-        closePopup.addEventListener('click', function (e) {
-            document.querySelectorAll('.popup-container').forEach((popups) =>{
-                popups.classList.remove('js-open');
-            });
+
+            // Remove active class from all tabs and content in the same group
+            document.querySelectorAll(`[data-href][data-group="${group}"]`).forEach(el => el.classList.remove("js-active"));
+            document.querySelectorAll(`[data-name][data-group="${group}"]`).forEach(el => el.classList.remove("js-active"));
+
+            // Add active class to the clicked tab and corresponding content
+            this.classList.add("js-active");
+            document.querySelector(`[data-name="${target}"][data-group="${group}"]`).classList.add("js-active");
         });
     });
 
 
     // copy to clipboard
-    var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
-    if (copyTextareaBtn) {
-        copyTextareaBtn.addEventListener('click', function(event) {
-            this.classList.add('js-done');
-            setTimeout(function () {
-                copyTextareaBtn.classList.remove('js-done');
-            }, 1000)
-            var copyTextarea = document.querySelector('.copy-code__area');
-            copyTextarea.focus();
-            copyTextarea.select();
+    document.querySelectorAll(".js-textareacopybtn").forEach(button => {
+        button.addEventListener("click", function () {
+            const textarea = this.closest(".copy-code").querySelector(".copy-code__area");
+            textarea.select();
+            document.execCommand("copy");
 
-            try {
-                var successful = document.execCommand('copy');
-                var msg = successful ? 'successful' : 'unsuccessful';
-                // console.log('Copying text command was ' + msg);
-            } catch (err) {
-                console.log('Unable to copy');
-            }
+            this.classList.add("js-done");
+            setTimeout(() => this.classList.remove("js-done"), 1500);
         });
-    }
+    });
 
-    function findAncestor (el, sel) {
-        while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el,sel)));
+
+    function findAncestor(el, sel) {
+        while (el && !el.matches(sel)) {
+            el = el.parentElement;
+        }
         return el;
     }
 
-    // show/hides block
-    document.querySelectorAll('.js-show-hide').forEach((toggleLink) =>{
-        toggleLink.addEventListener('click', function (e) {
-            findAncestor(toggleLink, '.toggle-box').classList.toggle('open');
+    document.querySelectorAll(".js-show-hide").forEach(toggleLink => {
+        toggleLink.addEventListener("click", function () {
+            findAncestor(this, ".toggle-box")?.classList.toggle("open");
         });
     });
 }
